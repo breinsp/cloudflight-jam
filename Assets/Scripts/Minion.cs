0f5136b4543fbc8992c857282b7e7ddf08eb9239
Julian Hautzmayer <julian@mercuryal.com>
2020-04-30 23:34:53 +0200

    added minion and minion spawner

---
+++ Assets/Scripts/Minion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public string name;
+    public TextMesh nameTag;
+
+    private bool isMoving;
+    private Vector3 direction;
+    private Vector3 goal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = RandomGoal();
+        isMoving = true;
+        nameTag.text = name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = goal - transform.position;
+        direction.y = 0f;
+        //Debug.DrawRay(transform.position, direction.normalized, Color.red);
+        if (Physics.Raycast(transform.position, direction.normalized, 1f)){
+            //Debug.Log(name + " - raycast");
+            goal = RandomGoal();
+        }
+        if (direction.magnitude < 0.01f)
+        {
+            goal = RandomGoal();
+        } 
+        else
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(name + " collided!!!");
+        goal = RandomGoal();
+    }
+
+    private Vector3 RandomGoal()
+    {
+        Vector2 vec = UnityEngine.Random.insideUnitCircle.normalized * UnityEngine.Random.Range(5, 20);
+        Vector3 newGoal = new Vector3(vec.x, 1, vec.y);
+        //Debug.DrawLine(new Vector3(transform.position.x, 1, transform.position.z), newGoal, Color.white, 20f);
+        return newGoal;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject minionPrefab;
+    public List<GameObject> minions;
+    public float spawnRadius;
+    public float spawnTimeDelta;
+    private float lastSpawnTimeDelta;
+    void Awake()
+    {
+        minions = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastSpawnTimeDelta += Time.deltaTime;
+        if (lastSpawnTimeDelta >= spawnTimeDelta)
+        {
+            SpawnMinion();
+            lastSpawnTimeDelta = 0;
+        }
+        Debug.DrawRay(transform.position, Vector3.forward * spawnRadius, Color.red);
+    }
+
+    private void SpawnMinion()
+    {
+        float angle = UnityEngine.Random.Range(0, 2*Mathf.PI);
+        float x = Mathf.Cos(angle);
+        float z = Mathf.Sin(angle);
+        Vector3 newPos = new Vector3(x * spawnRadius + transform.position.x, transform.position.y, z * spawnRadius + transform.position.z);
+        GameObject minion = Instantiate(minionPrefab, newPos, Quaternion.identity);
+        minion.transform.parent = transform;
+        minions.Add(minion);
+    }
+
+}
